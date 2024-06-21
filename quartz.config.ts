54706aeb6d9b682335cbba7da67c17f435db6bdd
Raphaël Delcros <rapha17180@gmail.com>
2024-06-22 01:05:08 +0200

    Quartz sync: Jun 22, 2024, 1:05 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -43,7 +43,7 @@ const config: QuartzConfig = {
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#c14343",
+          secondary: "#7b97aa",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.05)",
         },
